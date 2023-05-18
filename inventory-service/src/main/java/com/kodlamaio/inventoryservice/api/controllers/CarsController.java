b173badf1a20096bb34b8d8e5f101fb9d7c84c5c
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-05-18 18:31:29 +0300

    Update Inventory-Service with new client-oriented common producers

--- inventory-service/src/main/java/com/kodlamaio/inventoryservice/api/controllers/CarsController.java
+++ inventory-service/src/main/java/com/kodlamaio/inventoryservice/api/controllers/CarsController.java
@@ -1,5 +1,6 @@
 package com.kodlamaio.inventoryservice.api.controllers;
 
+import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
 import com.kodlamaio.inventoryservice.business.abstracts.CarService;
 import com.kodlamaio.inventoryservice.business.dto.requests.create.CreateCarRequest;
 import com.kodlamaio.inventoryservice.business.dto.requests.update.UpdateCarRequest;
@@ -28,6 +29,11 @@ public class CarsController {
     @GetMapping("/{id}")
     public GetCarResponse getById(UUID id) {
         return service.getById(id);
+    }
+
+    @GetMapping("/check-car-available/{id}")
+    public ClientResponse checkIfCarAvailable(@PathVariable UUID id) {
+        return service.checkIfCarAvailable(id);
     }
 
     @PostMapping
