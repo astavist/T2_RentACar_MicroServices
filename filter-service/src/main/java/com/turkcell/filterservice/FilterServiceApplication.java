24308684eb3aceab9356c8105d5cfb61cf5ab406
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-05-31 19:35:07 +0300

    Added Config-server and fixing yml docs

--- filter-service/src/main/java/com/turkcell/filterservice/FilterServiceApplication.java
+++ filter-service/src/main/java/com/turkcell/filterservice/FilterServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage, Paths.Filter.ServiceBasePackage})
 @EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage, Paths.Filter.ServiceBasePackage})
 public class FilterServiceApplication {
 
     public static void main(String[] args) {
